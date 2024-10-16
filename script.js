85e6def130cb56114f03291fb166b27baef99bc6
sputatrix <yogaamir91@gmail.com>
2024-10-16 21:49:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.title = 'sputatrix'
+const body = document.body
+const href1 = document.getElementById('href1')
+const href2 = document.getElementById('href2')
+const label1 = document.getElementById('label1')
+
+
+function clickKoleksi() {
+    alert(`BELOM ADA COY`)
+}
+
+function clickKategori() {
+    alert('BELOM ADA COYY')
+}
+
+function clickPencarian() {
+    alert('BELOM ADA COYYY')
+}
+
+function clickKategoriHewan() {
+    alert('LAGI DIBIKIN COY')
+}
+
+function clickKategoriUlar() {
+    alert('LAGI DIBIKIN COY')
+}
+
+function clickKategorKadal() {
+    alert('LAGI DIBIKIN COYY')
+}
+
+function clickKategoriExotic() {
+    alert('LAGI DIBIKIN COYYY')
+}
+
+function clickKategoriExoticLaut() {
+    alert('LAGI DIBIKIN COYYYY')
+}
